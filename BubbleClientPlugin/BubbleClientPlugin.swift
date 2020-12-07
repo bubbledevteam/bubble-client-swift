b0422365b4b8d9675d4d86b59a7842b1c61287ff
Yan <yan@YandeMacBook-Pro.local>
2020-12-07 16:26:28 +0800

    new loop

---
+++ BubbleClientPlugin/BubbleClientPlugin.swift
@@ -0,0 +1,31 @@
+//
+//  BubbleClientPlugin.swift
+//  BubbleClientPlugin
+//
+//  Created by Yan on 2020/12/7.
+//  Copyright © 2020 Mark Wilson. All rights reserved.
+//
+
+import Foundation
+import LoopKitUI
+import BubbleClient
+import BubbleClientUI
+import os.log
+
+class BubbleClientPlugin: NSObject, LoopUIPlugin {
+    
+    private let log = OSLog(category: "BubbleClientPlugin")
+    
+    public var pumpManagerType: PumpManagerUI.Type? {
+        return nil
+    }
+    
+    public var cgmManagerType: CGMManagerUI.Type? {
+        return BubbleClientManager.self
+    }
+    
+    override init() {
+        super.init()
+        log.default("BubbleClientPlugin Instantiated")
+    }
+}
